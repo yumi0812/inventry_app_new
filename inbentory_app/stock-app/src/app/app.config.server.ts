d67a7d9fccd32dcf274bc8c509ae91f2c31cc283
yumi0812 <sora111155@gmail.com>
2025-10-06 15:52:51 +0900

    Update app.config.server.ts

--- inbentory_app/stock-app/src/app/app.config.server.ts
+++ inbentory_app/stock-app/src/app/app.config.server.ts
@@ -1,11 +1,10 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering, withRoutes } from '@angular/ssr';
+import { provideServerRendering } from '@angular/ssr';
 import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering(withRoutes(serverRoutes))
+    provideServerRendering()
   ]
 };
 
